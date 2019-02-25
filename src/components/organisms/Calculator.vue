e07bdf18dabfb34b5d075da239e43f38ebf2fb86
tanabe-d <tanabe-d@gnavi.co.jp>
2019-02-25 18:49:55 +0900

    feat(Calculator): add calculator logic

--- src/components/organisms/Calculator.vue
+++ src/components/organisms/Calculator.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="calculator">
+    <div class="result">
+      <div class="result__formula">{{ formula }}</div>
+      <div class="result__answer">{{ answer }}</div>
+    </div>
+    <div class="clear">
+      <button class="button" type="button" @click="handleClickClear">Clear</button>
+    </div>
     <ol class="numbers">
       <li class="numbers__item" v-for="number in numbers">
-        <button class="button" type="button">{{ number }}</button>
+        <button class="button" type="button" @click="handleClickNumber">{{ number }}</button>
       </li>
     </ol>
     <ul class="operators">
       <li class="operators__item" v-for="operator in operators">
-        <button class="button" type="button">{{ operator }}</button>
+        <button v-if="operator === '＝'" class="button" type="button" @click="handleClickAnswer">{{ operator }}</button>
+        <button v-else class="button" type="button" @click="handleClickOperator">{{ operator }}</button>
       </li>
     </ul>
   </div>
@@ -19,16 +27,39 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 //
 @Component
 export default class Button extends Vue {
-  // https://github.com/kaorun343/vue-property-decorator#-propoptions-propoptions--constructor--constructor---decorator
-  @Prop({
-    type: Array,
-  })
-  private numbers: string[] = ['7', '8', '9', '4', '4', '6', '1', '2', '3', '0', '00', '.'];
+  // computed
+  get numbers(): string[] {
+    return this.$store.getters.numbers;
+  }
+  get operators(): string[] {
+    return this.$store.getters.operators;
+  }
+  get formula(): string {
+    return this.$store.getters.formula.length !== 0 ? this.$store.getters.formula.join('').replace(/_/g, ' ') : null;
+  }
+  get answer(): number {
+    return this.$store.getters.answer;
+  }
 
-  @Prop({
-    type: Array,
-  })
-  private operators: string[] = ['÷', '×', '＋', '−', '＝'];
+  // methods
+  public handleClickNumber(event: Event): void {
+    if (event.target instanceof HTMLElement) {
+      this.$store.dispatch('clickNumber', event.target.innerText);
+    }
+  }
+  public handleClickClear(): void {
+    this.$store.dispatch('clickClear');
+  }
+  public handleClickOperator(event: Event): void {
+    if (event.target instanceof HTMLElement) {
+      this.$store.dispatch('clickOperator', event.target.innerText);
+    }
+  }
+  public handleClickAnswer(event: Event): void {
+    if (event.target instanceof HTMLElement) {
+      this.$store.dispatch('clickAnswer', event.target.innerText);
+    }
+  }
 }
 </script>
 
@@ -37,17 +68,47 @@ export default class Button extends Vue {
   border: 1px solid #ccc;
   display: grid;
   grid-template-columns: 80% 20%;
+  grid-template-rows: 15% 85%;
+  grid-template-areas:
+    'result clear'
+    'numbers operators';
   height: 420px;
   width: 420px;
 }
 
+.result {
+  grid-area: result;
+  border: 1px solid #ccc;
+  display: grid;
+  grid-template-rows: 40% 60%;
+  text-align: right;
+  padding-right: 15%;
+}
+
+.result__formula {
+  color: #999;
+  font-size: 10px;
+  align-self: flex-end;
+}
+
+.result__answer {
+  font-size: 18px;
+  align-self: center;
+}
+
+.clear {
+  grid-area: clear;
+}
+
 .numbers {
+  grid-area: numbers;
   display: grid;
   grid-template-columns: 33.33% 33.33% 33.33%;
   grid-template-rows: 25% 25% 25% 25%;
 }
 
 .operators {
+  grid-area: operators;
   display: grid;
   grid-template-rows: 20% 20% 20% 20% 20%;
 }
--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,166 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    numbers: ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '00', '.'],
+    operators: ['÷', '×', '＋', '−', '＝'],
+    current: '',
+    formula: [],
+    answer: 0,
+    // 演算子が選択されてるか否か
+    isSelectedOperator: false,
+  },
+
+  getters: {
+    numbers(state): string[] {
+      return state.numbers;
+    },
+    operators(state): string[] {
+      return state.operators;
+    },
+    formula(state): string[] {
+      return state.formula;
+    },
+    answer(state): number {
+      return state.answer;
+    },
+  },
+  mutations: {
+    setNumber(state, payload): void {
+      // 直前の文字が等号の場合はformulaを空にする
+      if (state.current === '＝') {
+        state.formula = [];
+        state.answer = 0;
+      }
+
+      const { text }: { text: string } = payload;
+      const formula: string[] = state.formula;
+
+      formula.push(text);
+      state.isSelectedOperator = false;
+    },
+
+    setOperator(state, payload): void {
+      const { text }: { text: string } = payload;
+      const formula: string[] = state.formula;
+
+      // 演算子入力状態で再度演算子が入力されたら末尾演算子を更新する
+      if (state.isSelectedOperator) {
+        state.formula.pop();
+        formula.push(`_${text}_`);
+        return;
+      }
+
+      formula.push(text);
+      state.isSelectedOperator = true;
+    },
+
+    setAnswer(state): void {
+      const answer = state.formula
+        .join('')
+        .replace(/_/g, '')
+        .replace(/÷/g, '/')
+        .replace(/×/g, '*')
+        .replace(/＋/g, '+')
+        .replace(/−/g, '-');
+
+      state.answer = new Function(`return ${answer}`)();
+    },
+
+    clearSelectedNumbers(state): void {
+      state.current = '';
+      state.formula = [];
+      state.answer = 0;
+      state.isSelectedOperator = false;
+    },
+
+    setCurrent(state, payload): void {
+      const { text }: { text: string } = payload;
+      state.current = text;
+    },
+  },
+
+  actions: {
+    clickNumber({ commit, state }, text): void {
+      // 直前に入力された文字が空文字で、0かドットを入力した場合何も起こさない
+      if (state.current === '' && (text === '0' || text === '00' || text === '.')) {
+        return;
+      }
+
+      // 直前に入力された文字がドットで、ドットを入力した場合何も起こさない
+      if (state.current === '.' && text === '.') {
+        return;
+      }
+
+      // 直前に入力された文字が等号で、ドットか0を入力した場合何も起こさない
+      if (state.current === '＝' && (text === '.' || text === '0' || text === '00')) {
+        return;
+      }
+
+      // 演算子入力状態で、0かドットを入力した場合何も起こさない
+      if (state.isSelectedOperator && (text === '0' || text === '00' || text === '.')) {
+        return;
+      }
+
+      commit('setNumber', { text });
+      commit('setCurrent', { text });
+    },
+
+    clickOperator({ commit, state }, text): void {
+      const hasArithmetic = /[÷×＋−]/g.test(text);
+
+      // 直前に入力された文字が空文字で、四則演算が入力されている場合、何も起こさない
+      if (state.current === '' && hasArithmetic) {
+        return;
+      }
+
+      // 直前に入力された文字がドットで、四則演算が入力されている場合、何も起こさない
+      if (state.current === '.' && hasArithmetic) {
+        return;
+      }
+
+      // 直前に入力された文字が等号で、四則演算が入力されている場合、何も起こさない
+      if (state.current === '＝' && hasArithmetic) {
+        return;
+      }
+
+      commit('setOperator', { text });
+      commit('setCurrent', { text });
+    },
+
+    clickAnswer({ commit, state }, text): void {
+      // 直前に入力された文字がドットで、等号を入力した場合何も起こさない
+      if (state.current === '.' && text === '＝') {
+        return;
+      }
+
+      // 直前に入力された文字が等号で、等号を入力した場合何も起こさない
+      if (state.current === '＝' && text === '＝') {
+        return;
+      }
+
+      // 直前の文字が空文字の場合何も起こさない
+      if (state.current === '') {
+        return;
+      }
+
+      // 直前に四則演算が選択されている場合何も起こさない
+      if (state.isSelectedOperator) {
+        return;
+      }
+
+      // 四則演算が1つも式にない場合何も起こさない
+      if (!state.formula.some((formula) => /[÷×＋−]/g.test(formula))) {
+        return;
+      }
+
+      commit('setAnswer');
+      commit('setOperator', { text });
+      commit('setCurrent', { text });
+    },
+
+    clickClear({ commit }): void {
+      commit('clearSelectedNumbers');
+    },
+  },
 });
